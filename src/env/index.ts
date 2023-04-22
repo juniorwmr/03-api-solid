9d41b3d4780ca4c6c307585978f5b43feaf1ff03
Washington Muniz <juniorripardo@gmail.com>
2023-04-22 18:14:24 -0500

    feat: set up env variables

---
+++ src/env/index.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv'
+import { z } from 'zod'
+
+config()
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  HOST: z.string().default('0.0.0.0'),
+  PORT: z.number({
+    coerce: true
+  }).int().default(3333)
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid environment variables', _env.error.format())
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
